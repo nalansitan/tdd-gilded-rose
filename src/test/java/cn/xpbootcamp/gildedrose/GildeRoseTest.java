cd0d7b99e85e11e80485c21b183a115c8adb049e
nalansitan <nalansitan@gmail.com>
2020-05-14 22:41:05 +0800

    add Aged Brie test max quality

--- src/test/java/cn/xpbootcamp/gildedrose/GildeRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildeRoseTest.java
@@ -58,4 +58,20 @@ public class GildeRoseTest {
         Assert.assertEquals(5,gr.getQuality());
         Assert.assertEquals(-1,gr.getSellIn());
     }
+
+    @Test
+    public void should_return_50_when_given_sellin_0_quality_49_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(0,49, GildeRose.GOODSTYPES.AgedBrie);
+        gr.oneMoreDay();
+        Assert.assertEquals(50,gr.getQuality());
+        Assert.assertEquals(-1,gr.getSellIn());
+    }
+
+    @Test
+    public void should_return_50_when_given_sellin_10_quality_50_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(10,50, GildeRose.GOODSTYPES.AgedBrie);
+        gr.oneMoreDay();
+        Assert.assertEquals(50,gr.getQuality());
+        Assert.assertEquals(9,gr.getSellIn());
+    }
 }
