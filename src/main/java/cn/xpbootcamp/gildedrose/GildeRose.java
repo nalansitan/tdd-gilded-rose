04068e3a0990ce78aca5ea34af43548a0c4f25c1
nalansitan <nalansitan@gmail.com>
2020-05-14 22:51:45 +0800

    add BackStage Pass should_return_6_when_given_sellin_4_quality_3_good_type_backstage_pass

--- src/main/java/cn/xpbootcamp/gildedrose/GildeRose.java
+++ src/main/java/cn/xpbootcamp/gildedrose/GildeRose.java
@@ -5,12 +5,14 @@ import static cn.xpbootcamp.gildedrose.GildeRose.GOODSTYPES.Normal;
 public class GildeRose {
     private int SellIn;
     private int Quality;
-    public enum GOODSTYPES{
+
+    public enum GOODSTYPES {
         Sulfuras,
         Normal,
         AgedBrie,
         BackstagePass;
     }
+
     public GOODSTYPES GoodsTypes = Normal;
 
     public GildeRose(int SellIn, int Quality) {
@@ -30,9 +32,9 @@ public class GildeRose {
     }
 
     public int getQuality() {
-        if (this.Quality < 0){
+        if (this.Quality < 0) {
             this.Quality = 0;
-        }else if (this.Quality > 50){
+        } else if (this.Quality > 50) {
             this.Quality = 50;
         }
         return this.Quality;
@@ -43,33 +45,35 @@ public class GildeRose {
     }
 
     public void oneMoreDay() {
-        switch(this.GoodsTypes) {
+        switch (this.GoodsTypes) {
             case Sulfuras:
                 break;
             case AgedBrie:
                 this.SellIn -= 1;
-                if (this.SellIn < 0){
+                if (this.SellIn < 0) {
                     this.Quality += 2;
-                }else{
+                } else {
                     this.Quality += 1;
                 }
                 break;
             case BackstagePass:
                 this.SellIn -= 1;
-                if (this.SellIn < 0){
+                if (this.SellIn < 0) {
                     this.Quality = 0;
-                }else if (this.SellIn <=10){
+                } else if (this.SellIn <= 5) {
+                    this.Quality += 3;
+                } else if (this.SellIn <= 10) {
                     this.Quality += 2;
-                }else {
+                } else {
                     this.Quality += 1;
                 }
                 break;
             case Normal:
             default:
                 this.SellIn -= 1;
-                if (this.SellIn < 0){
+                if (this.SellIn < 0) {
                     this.Quality -= 2;
-                }else{
+                } else {
                     this.Quality += 1;
                 }
                 break;
--- src/test/java/cn/xpbootcamp/gildedrose/GildeRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildeRoseTest.java
@@ -90,4 +90,12 @@ public class GildeRoseTest {
         Assert.assertEquals(5,gr.getQuality());
         Assert.assertEquals(8,gr.getSellIn());
     }
+
+    @Test
+    public void should_return_6_when_given_sellin_4_quality_3_good_type_backstage_pass(){
+        GildeRose gr = new GildeRose(4,3, GildeRose.GOODSTYPES.BackstagePass);
+        gr.oneMoreDay();
+        Assert.assertEquals(6,gr.getQuality());
+        Assert.assertEquals(3,gr.getSellIn());
+    }
 }
