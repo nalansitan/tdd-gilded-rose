6cec2a4e1b6ba09dc78032d173897bb1dd3c88b2
nalansitan <nalansitan@gmail.com>
2020-05-14 21:56:36 +0800

    to check CI failed: add normal goods test should_return_1_when_given_sellin_0_quality_3

--- src/test/java/cn/xpbootcamp/gildedrose/GildeRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildeRoseTest.java
@@ -11,4 +11,12 @@ public class GildeRoseTest {
         Assert.assertEquals(4,gr.getQuality());
         Assert.assertEquals(9,gr.getSellIn());
     }
+
+    @Test
+    public void should_return_1_when_given_sellin_0_quality_3(){
+        GildeRose gr = new GildeRose(0,3);
+        gr.oneMoreDay();
+        Assert.assertEquals(1,gr.getQuality());
+        Assert.assertEquals(-1,gr.getSellIn());
+    }
 }
