9640323dae0d2aa0f35fde3427917377c42f4be5
nalansitan <nalansitan@gmail.com>
2020-05-17 19:24:59 +0800

    fix: update aged brie test case & tasking list

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -5,34 +5,42 @@ import org.junit.Test;
 
 public class AgedBrieTest {
     @Test
-    public void should_return_4_when_given_sellin_10_quality_3_good_type_aged_brie(){
-        GildeRose gr = new GildeRose(10,3, GildeRose.GOODSTYPES.AgedBrie);
+    public void should_return_1_when_given_sellin_2_quality_0_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(2,0, GildeRose.GOODSTYPES.AgedBrie);
         gr.oneMoreDay();
-        Assert.assertEquals(4,gr.getQuality());
-        Assert.assertEquals(9,gr.getSellIn());
+        Assert.assertEquals(1,gr.getQuality());
+        Assert.assertEquals(1,gr.getSellIn());
     }
 
     @Test
-    public void should_return_5_when_given_sellin_0_quality_3_good_type_aged_brie(){
-        GildeRose gr = new GildeRose(0,3, GildeRose.GOODSTYPES.AgedBrie);
+    public void should_return_50_when_given_sellin_2_quality_49_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(2,49, GildeRose.GOODSTYPES.AgedBrie);
         gr.oneMoreDay();
-        Assert.assertEquals(5,gr.getQuality());
+        Assert.assertEquals(50,gr.getQuality());
+        Assert.assertEquals(1,gr.getSellIn());
+    }
+
+    @Test
+    public void should_return_50_when_given_sellin_2_quality_50_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(2,50, GildeRose.GOODSTYPES.AgedBrie);
+        gr.oneMoreDay();
+        Assert.assertEquals(50,gr.getQuality());
+        Assert.assertEquals(1,gr.getSellIn());
+    }
+
+    @Test
+    public void should_return_22_when_given_sellin_0_quality_20_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(0,20, GildeRose.GOODSTYPES.AgedBrie);
+        gr.oneMoreDay();
+        Assert.assertEquals(22,gr.getQuality());
         Assert.assertEquals(-1,gr.getSellIn());
     }
 
     @Test
-    public void should_return_50_when_given_sellin_0_quality_49_good_type_aged_brie(){
-        GildeRose gr = new GildeRose(0,49, GildeRose.GOODSTYPES.AgedBrie);
+    public void should_return_22_when_given_sellin_minus_1_quality_20_good_type_aged_brie(){
+        GildeRose gr = new GildeRose(-1,20, GildeRose.GOODSTYPES.AgedBrie);
         gr.oneMoreDay();
-        Assert.assertEquals(50,gr.getQuality());
-        Assert.assertEquals(-1,gr.getSellIn());
-    }
-
-    @Test
-    public void should_return_50_when_given_sellin_10_quality_50_good_type_aged_brie(){
-        GildeRose gr = new GildeRose(10,50, GildeRose.GOODSTYPES.AgedBrie);
-        gr.oneMoreDay();
-        Assert.assertEquals(50,gr.getQuality());
-        Assert.assertEquals(9,gr.getSellIn());
+        Assert.assertEquals(22,gr.getQuality());
+        Assert.assertEquals(-2,gr.getSellIn());
     }
 }
